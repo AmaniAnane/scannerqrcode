fca08a0c69fae954188660ae9366c65f28e7035d
AmaniAnane <anane.amani.28@gmail.com>
2022-02-05 14:07:55 +0100

    authentification admin

---
+++ lib/screens/login.dart
@@ -0,0 +1,208 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import 'package:google_fonts/google_fonts.dart';
+import 'package:http/http.dart' as http;
+import 'package:scannerqrcode/screens/admin.dart';
+import 'package:scannerqrcode/screens/signup.dart';
+
+import 'dashboard.dart';
+
+class Signin extends StatefulWidget {
+  Signin({Key? key}) : super(key: key);
+
+  @override
+  _SigninState createState() => _SigninState();
+}
+
+class _SigninState extends State<Signin> {
+  final _formKey = GlobalKey<FormState>();
+  Future save() async {
+    Map data = {
+      'email': admin.email,
+      'password': admin.password,
+    };
+    var body = json.encode(data);
+    var dio = Dio();
+    var res = await dio.post(
+      'http://10.0.2.2:9087/signin',
+      options: Options(headers: {
+        HttpHeaders.contentTypeHeader: "application/json",
+      }),
+      data: body,
+    );
+
+    print(res.data);
+    Navigator.push(
+        context, new MaterialPageRoute(builder: (context) => Dashboard()));
+  }
+
+  Admin admin = Admin('', '');
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+      children: [
+        Positioned(
+            top: 0,
+            child: SvgPicture.asset(
+              'images/top.svg',
+              width: 400,
+              height: 150,
+            )),
+        Container(
+          alignment: Alignment.center,
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 150,
+                ),
+                Text(
+                  "Signin",
+                  style: GoogleFonts.pacifico(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 50,
+                      color: Color.fromARGB(150, 240, 243, 33)),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextFormField(
+                    controller: TextEditingController(text: admin.email),
+                    onChanged: (value) {
+                      admin.email = value;
+                    },
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Enter something';
+                      } else if (RegExp(
+                              r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                          .hasMatch(value!)) {
+                        return null;
+                      } else {
+                        return 'Enter valid email';
+                      }
+                    },
+                    decoration: InputDecoration(
+                        icon: Icon(
+                          Icons.email,
+                          color: Colors.blue,
+                        ),
+                        hintText: 'Enter Email',
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            borderSide: BorderSide(
+                                color: Color.fromARGB(255, 243, 222, 33))),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            borderSide: BorderSide(
+                                color: Color.fromARGB(255, 240, 243, 33))),
+                        errorBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            borderSide: BorderSide(
+                                color: Color.fromARGB(188, 244, 177, 54))),
+                        focusedErrorBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            borderSide: BorderSide(
+                                color: Color.fromARGB(218, 244, 184, 54)))),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextFormField(
+                    controller: TextEditingController(text: admin.email),
+                    onChanged: (value) {
+                      admin.email = value;
+                    },
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Enter something';
+                      }
+                      return null;
+                    },
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        icon: Icon(
+                          Icons.vpn_key,
+                          color: Colors.blue,
+                        ),
+                        hintText: 'Enter Password',
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            borderSide: BorderSide(color: Colors.blue)),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            borderSide: BorderSide(color: Colors.blue)),
+                        errorBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            borderSide: BorderSide(color: Colors.red)),
+                        focusedErrorBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            borderSide: BorderSide(color: Colors.red))),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(55, 16, 16, 0),
+                  child: Container(
+                    height: 50,
+                    width: 400,
+                    child: FlatButton(
+                        color: Colors.blue,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16.0)),
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            save();
+                          } else {
+                            print("not ok");
+                          }
+                        },
+                        child: Text(
+                          "Signin",
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        )),
+                  ),
+                ),
+                Padding(
+                    padding: const EdgeInsets.fromLTRB(95, 20, 0, 0),
+                    child: Row(
+                      children: [
+                        Text(
+                          "Not have Account ? ",
+                          style: TextStyle(
+                              color: Colors.black, fontWeight: FontWeight.bold),
+                        ),
+                        InkWell(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                new MaterialPageRoute(
+                                    builder: (context) => Signup()));
+                          },
+                          child: Text(
+                            "Signup",
+                            style: TextStyle(
+                                color: Colors.blue,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ],
+                    ))
+              ],
+            ),
+          ),
+        )
+      ],
+    ));
+  }
+}
